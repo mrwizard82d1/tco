0ab30fae4840c42f846bf315ecd7a4097976ce9f
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2018-02-12 21:10:52 -0600

    Write shortest path using Dijkstra's algorithm

--- tco/graph.py
+++ tco/graph.py
@@ -1,4 +1,5 @@
 import collections
+import sys
 
 
 class Graph(object):
@@ -17,8 +18,12 @@ class Graph(object):
         """Returns the neighbors of node u."""
         return self.adjacencyList[u]
 
+    def nodes(self):
+        """Return all the nodes in this graph."""
+        return set(self.adjacencyList.keys())
+
     @staticmethod
-    def construct_path(target, paths_to_source):
+    def construct_search_path(target, paths_to_source):
         """Construct the path from source to target using paths_to_source."""
         path = [target]
         while True:
@@ -45,7 +50,7 @@ class Graph(object):
         while to_visit:
             from_node = to_visit.popleft()
             if from_node == v:
-                return self.construct_path(v, travel_log)
+                return self.construct_search_path(v, travel_log)
 
             for to_node in self.neighbors(from_node).keys():
                 if to_node in visited:
@@ -61,4 +66,55 @@ class Graph(object):
         # visited all nodes but failed to find target
         return []
 
+    @staticmethod
+    def to_visit_closest_to_source(to_visit, distances_to_source):
+        """Return a node to be visited that is closest to the source node."""
+        return min(to_visit, key=lambda n: distances_to_source[n])
 
+    @staticmethod
+    def construct_shortest_path(target, paths_to_source):
+        """Return the shortest path from source to target using paths_to_source."""
+        u = target
+        target_to_source = [u]
+        while paths_to_source[u]:
+            target_to_source.append(paths_to_source[u])
+            u = paths_to_source[u]
+
+        return list(reversed(target_to_source))
+
+    def shortest_path(self, source, target, infinity=sys.maxsize):
+        """Return the lowest cost path from source to target using Dijkstra's algorithm.
+
+        The additional parameter, infinity, allows callers to specify a largest possible distance appropriate to their
+        problem. This parameter is necessary because python(3) no longer supports `sys.maxint`. The default value is
+        `sys.maxsize`. This value is a practical limit, but not actually a limit for python 3 integers. See
+        https://stackoverflow.com/questions/13795758/what-is-sys-maxint-in-python-3 for details.
+        """
+
+        to_visit = set()
+        distances_to_source = {}
+        paths_to_source = {}
+
+        for v in self.nodes():
+            distances_to_source[v] = infinity
+            paths_to_source[v] = None
+            to_visit.add(v)
+        distances_to_source[source] = 0  # fix distance of source from source
+
+        while to_visit:
+            u = self.to_visit_closest_to_source(to_visit, distances_to_source)
+
+            to_visit.remove(u)
+            if u == target:
+                if paths_to_source[target]:
+                    return self.construct_shortest_path(target, paths_to_source), distances_to_source[target]
+                return None
+
+            for v in self.neighbors(u):
+                candidate = distances_to_source[u] + self.adjacencyList[u][v]
+                if candidate < distances_to_source[v]:
+                    # if we found a sorter path to the source, update distances and paths
+                    distances_to_source[v] = candidate
+                    paths_to_source[v] = u
+
+        return None
--- test/tco/test_graph.py
+++ test/tco/test_graph.py
@@ -46,5 +46,42 @@ class TestGraphSearch(unittest.TestCase):
         self.assertEqual(sut.breadth_first_search('a', 'b'), ['a', 'b'])
 
 
+class TestGraphPath(unittest.TestCase):
+    """Test finding paths through the Graph."""
+
+    def test_empty_graph_dijkstra_is_none(self):
+        sut = tco.Graph()
+
+        self.assertEqual(sut.shortest_path('a', 'b'), None)
+
+    def test_single_edge_graph_shortest_path_first_to_another_is_none(self):
+        sut = tco.Graph()
+        sut.add_edge('a', 'b')
+
+        self.assertEqual(sut.shortest_path('a', 'c'), None)
+
+    def test_single_edge_graph_shortest_path_first_to_second_returns_first_to_second_path(self):
+        sut = tco.Graph()
+        sut.add_edge('a', 'b')
+
+        self.assertEqual(sut.shortest_path('a', 'b')[0], ['a', 'b'])
+
+    def test_single_edge_graph_shortest_path_first_to_second_returns_first_to_second_weight(self):
+        sut = tco.Graph()
+        sut.add_edge('a', 'b', weight=3)
+
+        self.assertEqual(sut.shortest_path('a', 'b')[1], 3)
+
+    def test_multiple_edge_graph_shortest_path_returns_shortest_path_and_cost(self):
+        sut = tco.Graph()
+        sut.add_edge('a', 'b', weight=3)
+        sut.add_edge('b', 'd', weight=3)
+        sut.add_edge('a', 'c', weight=4)
+        sut.add_edge('c', 'd', weight=3)
+        sut.add_edge('a', 'd', weight=7)
+
+        self.assertEqual(sut.shortest_path('a', 'd'), (['a', 'b', 'd'], 6))
+
+
 if __name__ == '__main__':
     unittest.main()
